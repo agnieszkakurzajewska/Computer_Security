0293b199774aa4b348fa08dfba1d0c1cea8ae5cd
agnieszkakurzajewska <agnieszkakurzajewska@hotmail.com>
2022-01-06 22:34:39 +0100

    init

--- l5/bank/README.md
+++
@@ -1,6 +0,0 @@
-# PGAdmin:
-- host name: localhost
-- port: 5432
-- db_name: postgres
-- user_name: postgres
-- password: postgres
--- l5/bank/login/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import login
-
-urlpatterns = [
-    path('', login.index, name='index'),
-]
